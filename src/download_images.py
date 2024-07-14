62841b84bc6f6240392026b4cab5501812dfe5f0
Marlon May <marlon.may@hotmail.de>
2024-07-14 19:08:36 +0200

    add downloader

--- src/download_images.py
+++ src/download_images.py
@@ -1,15 +1,51 @@
-from medmnist import BloodMNIST
+import argparse
+import os.path
+from medmnist import INFO
+import importlib
 from PIL import Image
 import numpy as np
 
+
+def get_dataset(dataset_name):
+    """Dynamically import the dataset class."""
+    module = importlib.import_module('medmnist')
+    class_name = dataset_name.capitalize()
+    class_name = class_name.replace('mnist', 'MNIST').replace('3d', '3D')
+    return getattr(module, class_name)
+
+
+def save_sample_image(dataset_name, split="test", image_size=28, sample_index=0):
+    DataClass = get_dataset(dataset_name)
+    dataset = DataClass(split=split, download=True, size=image_size)
+    image, label = dataset[sample_index]
+
+    if isinstance(image, np.ndarray):
+        if len(image.shape) == 3 and image.shape[0] == 3:  # If it's a 3D image (3 channels)
+            image = Image.fromarray((image.transpose(1, 2, 0) * 255).astype(np.uint8))
+        elif len(image.shape) == 3:  # If it is 3D
+            middle_slice = image.shape[0] // 2
+            image = Image.fromarray((image[middle_slice] * 255).astype(np.uint8))
+        else:
+            image = Image.fromarray((image[0] * 255).astype(np.uint8))
+
+    filename = os.path.join("data", dataset_name, f"{image_size}-{split}-{sample_index}.png")
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    image.save(filename)
+    print(f"Image saved as {filename}")
+
+
 if __name__ == "__main__":
-    # Use a valid size (28, 64, 128, or 224)
-    image_size = 28
-    dataset = BloodMNIST(split="test", download=True, size=image_size)
+    parser = argparse.ArgumentParser(description="Save a sample image from a MedMNIST dataset")
 
-    # Get the first image
-    image, label = dataset[0]
+    # Use the keys from INFO directly
+    dataset_choices = list(INFO.keys())
 
-    # Save the image
-    image.save(f"blood-{image_size}.png")
-    print(f"Image saved")
+    parser.add_argument("dataset", type=str, choices=[choice for choice in dataset_choices], help="Name of the MedMNIST dataset")
+    parser.add_argument("--size", type=int, choices=[28, 64, 128, 224], default=28, help="Image size (default: 28) options: 28, 64, 128, 224")
+    parser.add_argument("--split", choices=["train", "val", "test"], default="test", help="Dataset split (default: test)")
+    parser.add_argument("--index", type=int, default=0, help="Index of the sample image (default: 0)")
+
+    args = parser.parse_args()
+    dataset_name = next(name for name in dataset_choices if name.lower() == args.dataset)
+
+    save_sample_image(dataset_name, args.split, args.size, args.index)
